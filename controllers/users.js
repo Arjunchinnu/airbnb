08c51bcc1aa6ac63a5f238297e99e2bb20f33487
arjunchinnu604@gmail.com <user.email like : "arjunchinnu604@gmail.com>
2026-01-29 09:34:57 +0530

    fixing logical problem

--- controllers/users.js
+++ controllers/users.js
@@ -1,102 +1,19 @@
-// const User = require("../models/user");
-// module.exports.renderSignupForm = (req, res) => {
-//   res.render("users/signup.ejs");
-// };
+const User = require("../models/user"); // 👈 CRITICAL
 
-// // module.exports.signup = async (req, res) => {
-// //   try {
-// //     let { username, email, password } = req.body;
-// //     const newUser = new user({ username, email });
-// //     const registeredUser = await user.register(newUser, password);
-// //     console.log(registeredUser);
-// //     req.login(registeredUser, (err) => {
-// //       if (err) {
-// //         return next(err);
-// //       }
-// //       req.flash("success", "Welcome to Wanderland");
-// //       res.redirect("/listings");
-// //     });
-// //   } catch (err) {
-// //     req.flash("error", err.message);
-// //     res.redirect("/users/signup");
-// //   }
-// // };
-
-// // ✅ FIXED
-// module.exports.signup = async (req, res, next) => {
-//   // 👈 Add next
-//   try {
-//     let { username, email, password } = req.body;
-//     const newUser = new User({ username, email }); // 👈 Capital U
-//     const registeredUser = await newUser.register(newUser, password);
-//     console.log(registeredUser);
-//     req.login(registeredUser, (err) => {
-//       if (err) return next(err); // ✅ Now works
-//       req.flash("success", "Welcome to Wanderland");
-//       res.redirect("/listings");
-//     });
-//   } catch (err) {
-//     req.flash("error", err.message);
-//     res.redirect("/users/signup");
-//   }
-// };
-
-// module.exports.renderLoginForm = (req, res) => {
-//   res.render("users/login.ejs");
-// };
-
-// module.exports.login = async (req, res) => {
-//   req.flash("success", "Welcome back to Wanderlust");
-//   let redirectUrl = res.locals.redirectUrl || "/listings";
-//   res.redirect(redirectUrl);
-// };
-
-// module.exports.logout = (req, res, next) => {
-//   req.logout((err) => {
-//     if (err) {
-//       req.flash("error", "Some issue occurred during logout");
-//       return next(err);
-//     }
-//     req.flash("success", "You are logged out");
-//     res.redirect("/listings");
-//   });
-// };
-
-const user = require("../models/user");
 module.exports.renderSignupForm = (req, res) => {
-  res.render("users/signup.ejs");
+  res.render("users/signup");
 };
 
-// module.exports.signup = async (req, res) => {
-//   try {
-//     let { username, email, password } = req.body;
-//     const newUser = new user({ username, email });
-//     const registeredUser = await user.register(newUser, password);
-//     console.log(registeredUser);
-//     req.login(registeredUser, (err) => {
-//       if (err) {
-//         return next(err);
-//       }
-//       req.flash("success", "Welcome to Wanderland");
-//       res.redirect("/listings");
-//     });
-//   } catch (err) {
-//     req.flash("error", err.message);
-//     res.redirect("/users/signup");
-//   }
-// };
-
-// ✅ FIXED
 module.exports.signup = async (req, res, next) => {
-  // 👈 Add next
   try {
     let { username, email, password } = req.body;
-    const newUser = new User({ username, email }); // 👈 Capital U
-    const registeredUser = await newUser.register(newUser, password);
-    console.log(registeredUser);
+    const newUser = new User({ username, email });
+    const registeredUser = await User.register(newUser, password); // 👈 FIXED
+    console.log("✅ User registered:", registeredUser._id);
+
     req.login(registeredUser, (err) => {
-      if (err) return next(err); // ✅ Now works
-      req.flash("success", "Welcome to Wanderland");
+      if (err) return next(err);
+      req.flash("success", "Welcome to Wanderlust!");
       res.redirect("/listings");
     });
   } catch (err) {
@@ -106,22 +23,19 @@ module.exports.signup = async (req, res, next) => {
 };
 
 module.exports.renderLoginForm = (req, res) => {
-  res.render("users/login.ejs");
+  res.render("users/login", { error: req.flash("error") });
 };
 
 module.exports.login = async (req, res) => {
-  req.flash("success", "Welcome back to Wanderlust");
+  req.flash("success", "Welcome back!");
   let redirectUrl = res.locals.redirectUrl || "/listings";
   res.redirect(redirectUrl);
 };
 
 module.exports.logout = (req, res, next) => {
   req.logout((err) => {
-    if (err) {
-      req.flash("error", "Some issue occurred during logout");
-      return next(err);
-    }
-    req.flash("success", "You are logged out");
+    if (err) return next(err);
+    req.flash("success", "Logged out!");
     res.redirect("/listings");
   });
 };
